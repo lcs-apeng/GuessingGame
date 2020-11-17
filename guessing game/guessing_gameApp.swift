828bf8a880fbcc9180b01c6acb13317ad4d1fabb
ameliapeng <apeng@lcs.on.ca>
2020-11-17 14:40:50 -0500

    Initial Commit

---
+++ guessing game/guessing_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessing_gameApp.swift
+//  guessing game
+//
+//  Created by amelia peng on 2020-11-17.
+//
+
+import SwiftUI
+
+@main
+struct guessing_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
