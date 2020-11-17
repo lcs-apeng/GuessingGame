bc86c028521c9a7e28059414170efe9a73da80b8
ameliapeng <apeng@lcs.on.ca>
2020-11-17 15:33:46 -0500

    almost done with this app! got user guess to be checked for correct input type function. and provide appropriate feedback.

--- guessing game/ContentView.swift
+++ guessing game/ContentView.swift
@@ -12,6 +12,13 @@ struct ContentView: View {
     //The guess made by the user
     @State private var theUserGuess = ""
     
+    //The number that the user should guess,
+    
+    let target = Int.random(in: 1...100)
+    
+    //Feedback to user
+    @State private var feedback = ""
+    
     var body: some View {
        
         NavigationView {
@@ -22,15 +29,19 @@ struct ContentView: View {
                 .font(.title)
                 .padding([.top, .leading, .bottom], 20.0)
             
-            TextField("Enter you guess here...",
+            TextField("Enter you guess here.",
                       text: $theUserGuess)
                 .padding(.leading, 108.0)
             
             Button("Submit Guess") {
                 //Check the guess
+                checkGuess()
             }
             
             Text("You guessed \(theUserGuess).")
+                .font(.title)
+            
+            Text("\(feedback).")
                 .font(.title)
             
             Spacer()
@@ -40,6 +51,26 @@ struct ContentView: View {
             
         }
     }
+    
+    //check what the user guessed against the target
+    func checkGuess() {
+        
+        //See if the user give us a integer in the expected range.
+        guard let givenInteger = Int(theUserGuess) else {
+            feedback = "Please provide a integer between 1 and 100."
+            return
+        }
+        guard givenInteger > 0, givenInteger < 101 else {
+            feedback = "Please provide a integer between 1 and 100."
+            return
+        }
+        
+        
+        //Is the guess correct?
+        feedback = "You made a guess."
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
