3752164b283f6df306bbd225faa5096e761848a8
ameliapeng <apeng@lcs.on.ca>
2020-11-17 15:17:20 -0500

    finished user interphase

--- guessing game/ContentView.swift
+++ guessing game/ContentView.swift
@@ -17,19 +17,24 @@ struct ContentView: View {
         NavigationView {
         
         VStack{
-            
-        Text("Guessing Game")
         
-        Text("I'm choosing a number between 1 and 100. Guess what it is!")
+            Text("I'm choosing a number between 1 and 100. Guess what it is!")
+                .font(.title)
+                .padding([.top, .leading, .bottom], 20.0)
             
-            TextField("Enter you guess here",
+            TextField("Enter you guess here...",
                       text: $theUserGuess)
+                .padding(.leading, 108.0)
             
             Button("Submit Guess") {
                 //Check the guess
             }
             
-            Text("You Guessed \(theUserGuess).")
+            Text("You guessed \(theUserGuess).")
+                .font(.title)
+            
+            Spacer()
+            
         }
         .navigationTitle("Guesssing Game")
             
